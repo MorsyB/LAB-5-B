ff23af40750597c73d2c415c55f8bb50eaa76bbb
morsy <morsyb.6c@gmail.com>
2022-06-14 16:06:05 +0300

    first push

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-import pandas
+from pandas import DataFrame
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
 def read_data(filename):
-    data = open("data\\glass.data")
-    data = pandas.read_csv("data\\glass.data", header=None)
-    print(data.shape)
+    file = pandas.read_csv("data\\" + filename, header=None)
+    file = file.values
+    data, label = file[:, 1:-1], file[:, -1]
+
+    label = LabelEncoder().fit_transform(label)
+    print(f'data: {data}')
+    print(f'label: {label}')
+    return data, label
 
 
 if __name__ == '__main__':
-    read_data("qwe")
+    data, label = read_data("glass.data")
+    normalized = DataFrame(MinMaxScaler().fit_transform(data))
+
+    train, test, train_vec, test_vec = train_test_split(data, label, test_size=0.2, random_state=42)
